5661a7290d61a39e49a439b0ea37ca97a16967f6
Roshan11032005 <roshanzameer111000@gmail.com>
2025-02-04 21:10:36 +0530

    send

--- nutri/app/(tabs)/_layout.tsx
+++ nutri/app/(tabs)/_layout.tsx
@@ -2,18 +2,13 @@ import React from 'react';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Link, Tabs } from 'expo-router';
 import { Pressable, Text } from 'react-native';
-
-import {Colors} from '@/constants/Colors';
+import { Colors } from '@/constants/Colors';
 import { useColorScheme } from 'react-native';
 import { useClientOnlyValue } from '@/components/useClientOnlyValue';
 import { getAuth } from 'firebase/auth';
 import { router } from 'expo-router';
 
-// You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
-  color: string;
-}) {
+function TabBarIcon(props: { name: React.ComponentProps<typeof FontAwesome>['name']; color: string; }) {
   return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
 }
 
@@ -34,8 +29,6 @@ export default function TabLayout() {
     <Tabs
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        // Disable the static render of the header on web
-        // to prevent a hydration error in React Navigation v6.
         headerShown: useClientOnlyValue(false, true),
       }}>
       <Tabs.Screen
@@ -66,6 +59,7 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
         }}
       />
+    
     </Tabs>
   );
 }
